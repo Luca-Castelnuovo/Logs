d83e3bf5d444d5e645e7c2fc49585bd940a074b0
Luca Castelnuovo <ltcastelnuovo@gmail.com>
2019-01-11 17:30:51 +0100

    added login front-end

--- index.php
+++ index.php
@@ -48,3 +48,51 @@ if (isset($_GET['reset'])) {
 if (isset($_SESSION['logged_in']) && $_SESSION['logged_in']) {
     redirect('/home');
 }
+
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <!-- Config -->
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <link rel="manifest" href="/site.webmanifest"></link>
+    <title>Login || Logs</title>
+
+    <!-- SEO -->
+    <link href="https://logs.lucacastelnuovo.nl" rel="canonical">
+    <meta content="A system to store and access all my logs" name="description">
+
+    <!-- Icons -->
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+
+    <!-- Styles -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdn.lucacastelnuovo.nl/general/css/materialize.css">
+</head>
+
+<body>
+<div class="row">
+    <div class="col s12 m8 offset-m2 l4 offset-l4">
+        <div class="card">
+            <div class="card-action blue accent-4 white-text">
+                <h3>Logs</h3>
+            </div>
+            <div class="card-content">
+                <div class="row center">
+                    <a class="waves-effect waves-light btn-large blue accent-4" href="?authenticate">
+                        Login with LTC
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+    <script src="https://cdn.lucacastelnuovo.nl/general/js/materialize.js"></script>
+    <?= alert_display() ?>
+</body>
+
+</html>
